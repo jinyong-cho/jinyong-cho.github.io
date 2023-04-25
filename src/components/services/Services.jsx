62ed43b662e4ec666126c649fb7ba3c879a5d8d4
Jinyong Cho <60642590+jinyong-cho@users.noreply.github.com>
2023-04-26 05:34:33 +0900

    Added new images and components

---
+++ src/components/services/Services.jsx
@@ -0,0 +1,164 @@
+import React, { useState } from 'react';
+import './services.css';
+
+const Services = () => {
+  const [toggleState, setToggleState] = useState(0);
+
+  const toggleTab = (index) => {
+    setToggleState(index);
+  };
+
+  return (
+    <section className="services section" id="services">
+      <h2 className="section__title">Services</h2>
+      <span className="section__subtitle">What i offer</span>
+
+      <div className="services__container container grid">
+        <div className="services__content">
+          <div>
+            <i className="uil uil-web-grid services__icon"></i>
+            <h3 className="services__title">
+              JavaScript <br /> Developer
+            </h3>
+          </div>
+
+          <span onClick={() => toggleTab(1)} className="services__button">
+            View More
+            <i className="uil uil-arrow-right services__button-icon"></i>
+          </span>
+
+          <div
+            className={
+              toggleState === 1
+                ? 'services__modal active-modal'
+                : 'services__modal'
+            }
+          >
+            <div className="services__modal-content">
+              <i
+                onClick={() => toggleTab(0)}
+                className="uil uil-times services__modal-close"
+              ></i>
+
+              <h3 className="services__modal-title">JavaScript Developer</h3>
+              <p className="services__modal-description">
+                Service with more than 3 years of experience. Providing quality
+                work to clients and companies
+              </p>
+
+              <ul className="services__modal-services grid">
+                <li className="services__modal-service grid">
+                  <i className="uil uil-check-circle services__modal-icon"></i>
+                  <p className="services__modal-info">
+                    I develop the user interface.
+                  </p>
+                </li>
+
+                <li className="services__modal ">
+                  <i className="uil uil-check-circle services__modal-icon"></i>
+                  <p className="services__modal-info">Web page development.</p>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+
+        <div className="services__content">
+          <div>
+            <i className="uil uil-arrow services__icon"></i>
+            <h3 className="services__title">
+              HTML / CSS
+              <br />
+              Developer
+            </h3>
+          </div>
+
+          <span onClick={() => toggleTab(2)} className="services__button">
+            View More
+            <i className="uil uil-arrow-right services__button-icon"></i>
+          </span>
+
+          <div
+            className={
+              toggleState === 2
+                ? 'services__modal active-modal'
+                : 'services__modal'
+            }
+          >
+            <div className="services__modal-content">
+              <i
+                onClick={() => toggleTab(0)}
+                className="uil uil-times services__modal-close"
+              ></i>
+
+              <h3 className="services__modal-title">HTML / CSS Developer</h3>
+              <p className="services__modal-description">
+                Service with more than 3 years of experience. Providing quality
+                work to clients and companies
+              </p>
+
+              <ul className="services__modal-services grid">
+                <li className="services__modal-service">
+                  <i className="uil uil-check-circle services__modal-icon"></i>
+                  <p className="services__modal-info">
+                    I develop the user interface.
+                  </p>
+                </li>
+
+                <li className="services__modal-service">
+                  <i className="uil uil-check-circle services__modal-icon"></i>
+                  <p className="services__modal-info">Web page development.</p>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+
+        <div className="services__content">
+          <div>
+            <i className="uil uil-edit services__icon"></i>
+            <h3 className="services__title">DataBase</h3>
+          </div>
+
+          <span onClick={() => toggleTab(3)} className="services__button">
+            View More
+            <i className="uil uil-arrow-right services__button-icon"></i>
+          </span>
+          <div
+            className={
+              toggleState === 3
+                ? 'services__modal active-modal'
+                : 'services__modal'
+            }
+          >
+            <div className="services__modal-content">
+              <i
+                onClick={() => toggleTab(0)}
+                className="uil uil-times services__modal-close"
+              ></i>
+
+              <h3 className="services__modal-title">DataBase</h3>
+              <p className="services__modal-description">
+                Service with more than 3 years of experience. Providing quality
+                work to clients and companies
+              </p>
+
+              <ul className="services__modal-services grid">
+                <li className="services__modal-service">
+                  <i className="uil uil-check-circle services__modal-icon"></i>
+                  <p className="services__modal-info">데이터베이스입니다.</p>
+                </li>
+
+                <li className="services__modal-service">
+                  <i className="uil uil-check-circle services__modal-icon"></i>
+                  <p className="services__modal-info">데이터베이스입니다.</p>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+export default Services;
---
+++ src/components/testimonials/Data.jsx
@@ -0,0 +1,28 @@
+import Image1 from '../../assets/HTML.jpg';
+import Image2 from '../../assets/CSS.jpg';
+import Image3 from '../../assets/JS.jpg';
+
+export const Data = [
+  {
+    id: 1,
+    image: Image1,
+    title: 'HTML',
+    description: '웹사이트의 모습을 기술하기 위한 마크업 언어.',
+  },
+
+  {
+    id: 2,
+    image: Image2,
+    title: 'CSS',
+    description:
+      'HTML 등의 마크업 언어로 작성된 문서가 실제로 웹사이트에 표현되는 방법을 정해주는 스타일 시트 언어.',
+  },
+
+  {
+    id: 3,
+    image: Image3,
+    title: 'JavaScript',
+    description:
+      '웹 페이지에서 사용자로부터 특정 이벤트나 입력 값을 받아 동적인 처리를 목적으로 고안된 객체 기반의 스크립트 프로그래밍 언어.',
+  },
+];
